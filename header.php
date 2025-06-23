b82d3a08da791a8021ef105a1a20d8c6db91d8f3
Tim <github@timgreen.ws>
2025-06-23 17:29:15 +1000

    added header

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo('charset'); ?>" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<div id="wrapper" class="hfeed">
+    <header id="header">
+        <div id="branding">
+            <div id="site-title">
+                <h1>
+                    <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></a>
+                </h1>
+                <div id="site-description"><?php bloginfo('description'); ?></div>
+            </div>
+        </div>
+        <nav id="menu">
+         <?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
+        </nav>
+    </header>
+<div id="container">
