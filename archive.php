b00610620daad091a98bb3ef542d2ece6cff1cad
Tim <github@timgreen.ws>
2025-06-23 17:27:17 +1000

    added archive

---
+++ archive.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+        <header class="header">
+            <h2 class="entry-title"><?php the_archive_title(); ?></h2>
+        </header>
+        <main id="content">
+            <?php 
+                if ( have_posts() ) : while ( have_posts() ) : the_post();
+                get_template_part( 'entry-summary' ); 
+                endwhile; endif;
+                dutch_page_navigation(); 
+            ?>
+        </main>
+    <?php 
+        get_sidebar(); 
+        get_footer(); 
+    ?>
