d9cf4bcfc7d95fe37d65e078affa95a586a57d13
Tim <github@timgreen.ws>
2025-06-23 20:24:05 +1000

    added theme support

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+
+// Setup the dutch theme 
+function dutch_setup() {
+	add_theme_support( 'title-tag' );
+	add_theme_support( 'post-thumbnails' );
+	add_theme_support( 'automatic-feed-links' );
+	add_theme_support( 'responsive-embeds' );
+	add_theme_support( 'disable-custom-colors' );
+	add_theme_support( 'editor-color-palette', array() );
+	add_theme_support( 'disable-custom-gradients' );
+	add_theme_support( 'editor-gradient-presets', array() );
+	add_theme_support( 'html5', array('search-form','comment-form','comment-list','gallery','caption','script','style',));
+
