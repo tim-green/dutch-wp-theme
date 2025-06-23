871cebb6c81dff9dcaf1a8b97092e126020e9808
Tim <github@timgreen.ws>
2025-06-23 18:25:09 +1000

    cleaned up

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
-        ?>    </main><?php get_footer(); ?><?php get_header(); ?>
+<?php get_header(); ?>
     <main id="content">
         <?php 
             if ( have_posts() ) : while ( have_posts() ) : the_post(); 
             get_template_part( 'entry-summary' );
             endwhile; endif; 
-        ?>    </main><?php get_footer(); ?>
+            dutch_page_navigation(); 
+        ?>
+    </main>
+<?php get_footer(); ?>
