7e5dcaf75395549f3f58e2d741506c8482c1b5b6
Tim <github@timgreen.ws>
2025-06-23 15:39:19 +1000

    added footer

---
+++ footer.php
@@ -0,0 +1,7 @@
+    <div class="clear"></div>
+    </div>
+            <footer id="footer">
+        <?php wp_footer(); ?>    </body></html>            </footer>
+        </div>
+        <?php wp_footer(); ?>
+    </body>
