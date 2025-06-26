6ab3c8f279b93883301a7c388cbbb2d1592ba8b8
Tim <github@timgreen.ws>
2025-06-26 17:02:55 +1000

    cleaned up footer

--- footer.php
+++ footer.php
@@ -1,7 +1,17 @@
     <div class="clear"></div>
     </div>
             <footer id="footer">
-        <?php wp_footer(); ?>    </body></html>            </footer>
+                <div id="search">
+                    <?php get_search_form(); ?>
+                </div>
+                <div id="copyright">
+                    <?php bloginfo( 'name' ); ?>
+                    <span class="post-count"> website contains <?php $count_posts = wp_count_posts(); $total_posts = $count_posts->publish; echo $total_posts . ' posts'; ?>
+                     &copy;  <?php echo date( "Y" ); ?> 
+                </span>
+                </div>
+            </footer>
         </div>
         <?php wp_footer(); ?>
     </body>
+</html>
