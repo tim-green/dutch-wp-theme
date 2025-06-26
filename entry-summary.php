42a0d7a31ade22226b9c0a689cd538cf81d6f5cb
Tim <github@timgreen.ws>
2025-06-26 16:53:15 +1000

    added entry summary

---
+++ entry-summary.php
@@ -0,0 +1,16 @@
+<div class="entry-summary">
+    <a href="<?php the_permalink(); ?>" rel="bookmark">
+        <?php the_title( '<h2 class="entry-title-summary">', '</h2>' ); ?>
+        <div class="entry-meta">
+            <span class="entry-date">
+            <?php   
+                    the_time( get_option( 'date_format' ) );  
+                    $up_time = get_the_time('U'); 
+                    $modified_time = get_the_modified_time('U'); 
+                    if ($modified_time >= $up_time + 86400) { echo ", Updated: "; 
+                    the_modified_time( get_option( 'date_format' ) ); } 
+            ?>
+            </span>
+        </div>
+    </a>
+</div>
